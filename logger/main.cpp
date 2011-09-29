a8c5ffa6125037a9e4e13cf567644f9bf0f7c822
Johannes Wienke <jwienke@techfak.uni-bielefeld.de>
2011-09-29 10:13:39 +0000

    fix indentation

--- logger/main.cpp
+++ logger/main.cpp
@@ -62,7 +62,7 @@ public:
     }
 
     string getClassName() const {
-	return "FormattingHandler";
+    return "FormattingHandler";
     }
 private:
     EventFormatterPtr formatter;
@@ -72,9 +72,9 @@ template <typename WireType>
 typename ConverterSelectionStrategy<WireType>::Ptr createConverterSelectionStrategy() {
     list< pair<ConverterPredicatePtr, typename Converter<WireType>::Ptr> > converters;
     converters.push_back(make_pair(ConverterPredicatePtr(new RegexConverterPredicate("(utf-8|ascii)-string")),
-				   typename Converter<WireType>::Ptr(new StringConverter())));
+                   typename Converter<WireType>::Ptr(new StringConverter())));
     converters.push_back(make_pair(ConverterPredicatePtr(new AlwaysApplicable()),
-				   typename Converter<WireType>::Ptr(new ByteArrayConverter())));
+                   typename Converter<WireType>::Ptr(new ByteArrayConverter())));
     return typename ConverterSelectionStrategy<WireType>::Ptr(new PredicateConverterList<WireType>(converters.begin(), converters.end()));
 }
 
@@ -85,33 +85,33 @@ options_description options("Allowed options");
 
 bool handleCommandline(int argc, char *argv[]) {
     options.add_options()
-	("help",
-	 "Display a help message.")
-	("scope",
-	 value<string>(&scope),
-	 "The scope of the channel for which events should be logged.")
-	("format",
-	 value<string>(&eventFormat)->default_value("compact"),
-	 str(format("The format that should be used to print received events. Value has to be one of %1%.")
-	     % getEventFormatterNames()).c_str());
+    ("help",
+     "Display a help message.")
+    ("scope",
+     value<string>(&scope),
+     "The scope of the channel for which events should be logged.")
+    ("format",
+     value<string>(&eventFormat)->default_value("compact"),
+     str(format("The format that should be used to print received events. Value has to be one of %1%.")
+         % getEventFormatterNames()).c_str());
 
     positional_options_description positional_options;
     positional_options.add("scope", 1);
 
     variables_map map;
     store(command_line_parser(argc, argv)
-	  .options(options)
-	  .positional(positional_options)
-	  .run(), map);
+      .options(options)
+      .positional(positional_options)
+      .run(), map);
     notify(map);
     if (map.count("help"))
-	return true;
+        return true;
     if (!getEventFormatterNames().count(eventFormat)) {
-	throw invalid_argument(str(format("Argument of --format option has to one of %1%.")
-				   % getEventFormatterNames()));
+        throw invalid_argument(str(format("Argument of --format option has to one of %1%.")
+                   % getEventFormatterNames()));
     }
     if (scope.empty()) {
-	throw invalid_argument("A Scope has to be specified.");
+        throw invalid_argument("A Scope has to be specified.");
     }
 
     return false;
@@ -135,14 +135,14 @@ void handleSIGINT(int /*signal*/) {
 int main(int argc, char* argv[]) {
     // Handle commandline arguments.
     try {
-	if (handleCommandline(argc, argv)) {
-	    usage(); // --help
-	    return EXIT_SUCCESS;
-	}
+        if (handleCommandline(argc, argv)) {
+            usage(); // --help
+            return EXIT_SUCCESS;
+        }
     } catch (const std::exception& e) {
-	cerr << "Error parsing command line: " << e.what() << endl;
-	usage();
-	return EXIT_FAILURE;
+        cerr << "Error parsing command line: " << e.what() << endl;
+        usage();
+        return EXIT_FAILURE;
     }
 
     // Create an event formatter
@@ -152,21 +152,21 @@ int main(int argc, char* argv[]) {
 
     // Configure a Listener object.
     ParticipantConfig config
-	= Factory::getInstance().getDefaultParticipantConfig();
+        = Factory::getInstance().getDefaultParticipantConfig();
     ParticipantConfig::Transport transport = config.getTransport("spread");
     Properties options = transport.getOptions();
     options["converters"] = createConverterSelectionStrategy<string>();
     transport.setOptions(options);
     config.addTransport(transport);
     ListenerPtr listener
-	= Factory::getInstance().createListener(Scope(scope), config);
+        = Factory::getInstance().createListener(Scope(scope), config);
     listener->addHandler(HandlerPtr(new FormattingHandler(formatter)));
 
     // Wait until termination is requested through the SIGINT handler.
     signal(SIGINT, &handleSIGINT);
     while (!doTerminate) {
-	recursive_mutex::scoped_lock lock(terminateMutex, try_to_lock);
-	terminateCondition.wait(lock);
+        recursive_mutex::scoped_lock lock(terminateMutex, try_to_lock);
+        terminateCondition.wait(lock);
     }
 
     return EXIT_SUCCESS;
