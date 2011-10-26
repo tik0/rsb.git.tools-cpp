ac9f208c3cd6530807aa87db8644cf1b60f3bb15
Johannes Wienke <jwienke@techfak.uni-bielefeld.de>
2011-10-26 07:08:25 +0000

    add a test case where one scope is published with double frequency, but all are published in sync.

--- test/rsbtimesync/ApproximateTimeStrategyTest.cpp
+++ test/rsbtimesync/ApproximateTimeStrategyTest.cpp
@@ -55,22 +55,30 @@ private:
 
 };
 
-TEST(ApproximateTimeStrategyTest, testEqualFrequencyAndTiming) {
+class ApproximateTimeStrategyTest: public ::testing::Test {
+public:
 
+	boost::shared_ptr<ApproximateTimeStrategy> strategy;
+	boost::shared_ptr<StoringSyncDataHandler> handler;
 	set<Scope> scopes;
-	scopes.insert("/aaa");
-	scopes.insert("/bbb");
-	scopes.insert("/ccc");
 
-	boost::shared_ptr<StoringSyncDataHandler> handler(
-			new StoringSyncDataHandler);
+	void SetUp() {
+		strategy.reset(new ApproximateTimeStrategy());
+		handler.reset(new StoringSyncDataHandler);
+		strategy->setSyncDataHandler(handler);
+		scopes.clear();
+		scopes.insert("/aaa");
+		scopes.insert("/bbb");
+		scopes.insert("/ccc");
+		strategy->initializeChannels(*(scopes.begin()), scopes);
+	}
+};
 
-	ApproximateTimeStrategy strategy;
-	strategy.setSyncDataHandler(handler);
-	strategy.initializeChannels(*(scopes.begin()), scopes);
+TEST_F(ApproximateTimeStrategyTest, testEqualFrequencyAndTiming) {
 
 	// very simple case, completely synchronized events
-	for (unsigned int i = 0; i < 5; ++i) {
+	unsigned int totalIterations = 5;
+	for (unsigned int i = 0; i < totalIterations; ++i) {
 
 		for (set<Scope>::const_iterator scopeIt = scopes.begin();
 				scopeIt != scopes.end(); ++scopeIt) {
@@ -78,22 +86,25 @@ TEST(ApproximateTimeStrategyTest, testEqualFrequencyAndTiming) {
 			EventPtr event(new Event);
 			event->setEventId(rsc::misc::UUID(), i);
 			event->setScope(*scopeIt);
-			strategy.handle(event);
+			event->setType(rsc::runtime::typeName<string>());
+			event->setData(
+					VoidPtr(new string(boost::str(boost::format("%d") % i))));
+			strategy->handle(event);
 
 			boost::this_thread::sleep(boost::posix_time::milliseconds(10));
 
 		}
 
-		boost::this_thread::sleep(boost::posix_time::seconds(1));
+		boost::this_thread::sleep(boost::posix_time::milliseconds(50));
 
 	}
 
-	EXPECT_EQ(size_t(4), handler->getEvents().size())
+	EXPECT_EQ(size_t(totalIterations - 1), handler->getEvents().size())
 		<< "For equal timing of all scopes the algorithm can always "
 				"produce number of published sequences - 1 sync sets. "
 				"For the last it waits until another element is "
 				"available on each scope, because otherwise the "
-				"condition that T_m is the Pivot cannot be checked.";
+				"condition that T_m is the pivot cannot be checked.";
 
 	for (unsigned int sequenceNumber = 0;
 			sequenceNumber < handler->getEvents().size(); ++sequenceNumber) {
@@ -105,7 +116,70 @@ TEST(ApproximateTimeStrategyTest, testEqualFrequencyAndTiming) {
 		EXPECT_EQ(size_t(3), data->size());
 		for (set<Scope>::const_iterator scopeIt = scopes.begin();
 				scopeIt != scopes.end(); ++scopeIt) {
-			EXPECT_EQ(size_t(1), data->count(*scopeIt));
+			ASSERT_EQ(size_t(1), data->count(*scopeIt));
+			string expectd = boost::str(boost::format("%d") % sequenceNumber);
+			string actual = *(boost::static_pointer_cast<string>(
+					data->at(*scopeIt).front()->getData()));
+			EXPECT_EQ(expectd, actual);
+		}
+
+	}
+
+}
+
+TEST_F(ApproximateTimeStrategyTest, testOneDoubleFrequency) {
+
+	const Scope doubleScope = *(scopes.begin());
+
+	unsigned int totalIterations = 10;
+	for (unsigned int i = 0; i < totalIterations; ++i) {
+
+		for (set<Scope>::const_iterator scopeIt = scopes.begin();
+				scopeIt != scopes.end(); ++scopeIt) {
+
+			if (i % 2 == 0 || *scopeIt == doubleScope) {
+
+				EventPtr event(new Event);
+				event->setEventId(rsc::misc::UUID(), i);
+				event->setScope(*scopeIt);
+				event->setType(rsc::runtime::typeName<string>());
+				event->setData(
+						VoidPtr(
+								new string(
+										boost::str(boost::format("%d") % i))));
+				strategy->handle(event);
+
+				boost::this_thread::sleep(boost::posix_time::milliseconds(10));
+
+			}
+
+		}
+
+		boost::this_thread::sleep(boost::posix_time::milliseconds(50));
+
+	}
+
+	EXPECT_EQ(size_t(totalIterations / 2 - 1), handler->getEvents().size())
+		<< "For equal timing of all scopes the algorithm can always "
+				"produce number of published sequences - 1 sync sets. "
+				"For the last it waits until another element is "
+				"available on each scope, because otherwise the "
+				"condition that T_m is the Pivot cannot be checked.";
+
+	for (unsigned int i = 0; i < handler->getEvents().size(); ++i) {
+
+		boost::shared_ptr<SyncMapConverter::DataMap> data =
+				boost::static_pointer_cast<SyncMapConverter::DataMap>(
+						handler->getEvents()[i]->getData());
+
+		EXPECT_EQ(size_t(3), data->size());
+		for (set<Scope>::const_iterator scopeIt = scopes.begin();
+				scopeIt != scopes.end(); ++scopeIt) {
+			ASSERT_EQ(size_t(1), data->count(*scopeIt));
+			string expectd = boost::str(boost::format("%d") % (i * 2));
+			string actual = *(boost::static_pointer_cast<string>(
+					data->at(*scopeIt).front()->getData()));
+			EXPECT_EQ(expectd, actual);
 		}
 
 	}
